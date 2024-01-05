0c77c3f697013f961cfe4d21d36d9e6cfda378d5
Hhummann <hhummannv@gmail.com>
2024-01-05 21:37:31 +0100

    Add minimalistic Steam communication
    Just initiate Steam connection and add function to plain text search
    user

--- src/bot/bot_base.py
+++ src/bot/bot_base.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 
 
 class BotBase(commands.Bot):
-
     def __init__(self):
         super(BotBase, self).__init__(intents=Intents.all(), command_prefix="!", help_command=None)
         self.application_token = "MTE5MTUwMDcyMTQwNTgyNTEwNg.GCnvQC.2XT-zZql3zuEEMipKC8PApe8XzauAjEXJMXoMg"
